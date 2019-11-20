bee76bcfeb223930d9c9f47cf0280aeb7b908b5f
T.K. Kwon <34696882+tkwon98@users.noreply.github.com>
2019-11-20 02:24:42 -0500

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -111,10 +111,41 @@ window.onload=function(){
         // swiped left
           window.alert("left");
         console.log("swiped left");
+          
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    const currentCaption = caption_track.querySelector('.current-caption');
+    const prevCaption = currentCaption.previousElementSibling;
+    currentCaption.classList.add('is-hidden');
+    currentCaption.classList.remove('current-caption');
+    prevCaption.classList.add('current-caption');
+    prevCaption.classList.remove('is-hidden');
+
+    moveToSlide(track, currentSlide, prevSlide);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+
       } else {
         // swiped right
           window.alert("right");
         console.log("swiped right");
+          
+    const currentSlide = track.querySelector('.current-slide');
+    const currentCaption = caption_track.querySelector('.current-caption');
+    const nextCaption = currentCaption.nextElementSibling;
+    currentCaption.classList.add('is-hidden');
+    currentCaption.classList.remove('current-caption');
+    nextCaption.classList.add('current-caption');
+    nextCaption.classList.remove('is-hidden');
+
+    const nextSlide = currentSlide.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+
+
       }  
     } else {
       // sliding vertically
