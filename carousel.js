268f1843da10712c5403b0424253be03fe4d3177
T.K. Kwon <34696882+tkwon98@users.noreply.github.com>
2019-11-20 02:17:34 -0500

    Add files via upload

--- carousel.js
+++ carousel.js
@@ -71,3 +71,66 @@ nextButton.addEventListener('click', e => {
     hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
+
+
+window.onload=function(){
+  const swipeArea = document.querySelector(".fullscreen");
+
+    swipeArea.addEventListener("touchstart", startTouch);
+    swipeArea.addEventListener("touchmove", moveTouch);
+}
+
+
+  // Swipe Up / Down / Left / Right
+  var initialX = null;
+  var initialY = null;
+
+  function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+  };
+
+  function moveTouch(e) {
+    if (initialX === null) {
+      return;
+    }
+
+    if (initialY === null) {
+      return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      // sliding horizontally
+      if (diffX > 0) {
+        // swiped left
+          window.alert("sometext");
+        console.log("swiped left");
+      } else {
+        // swiped right
+          window.alert("sometext");
+        console.log("swiped right");
+      }  
+    } else {
+      // sliding vertically
+      if (diffY > 0) {
+        // swiped up
+          window.alert("sometext");
+        console.log("swiped up");
+      } else {
+        // swiped down
+          window.alert("sometext");
+        console.log("swiped down");
+      }  
+    }
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+  };
